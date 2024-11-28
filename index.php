dec837591f799b949f91516fc5a97369b59b259e
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-11-28 23:59:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-burh
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web2A</title>
+</head>
+<body>
+  <h1>Table of contents</h1>
+  <p>Navigate to one of the following projects stored in this repository:</p>
+  <ul>
+    <li>
+      Nothing here yet
+    </li>
+  </ul>
+</body>
+</html>
