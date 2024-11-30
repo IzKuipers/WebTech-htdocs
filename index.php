052d88736dee381d56cbfed71dd96956b0c7f543
Izaak Kuipers <izaak.kuipers@gmail.com>
2024-11-30 23:13:58 +0100

    Add spaceships to main index file

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Web2A</title>
 </head>
+
 <body>
   <h1>Table of contents</h1>
   <p>Navigate to one of the following projects stored in this repository:</p>
@@ -18,6 +20,10 @@
     <li>
       Klachtverwerking v2: <a href="/school/klachtverwerking-extended">/klachtverwerking-extended</a>
     </li>
+    <li>
+      Spaceships: <a href="/school/spaceships">/spaceships</a>
+    </li>
   </ul>
 </body>
+
 </html>
